12f75ba18d944082205b583e7908251fff508a8c
Willianson5522 <williansoncesar@gmail.com>
2022-11-02 12:04:52 -0300

    display snake function

--- game.js
+++ game.js
@@ -1,17 +1,27 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-function drawGame() {
-  let speed = 7; //The interval will be seven times a second
+let tileCount = 20; //define screen size in pixels
+let tileSize = 18; //define snake initial size
+let headX = 10;
+let headY = 10;
 
+function drawGame() {
+  clearScreen();
+  drawSnake();
+
+  let speed = 7; //The interval will be seven times a second
   setTimeout(drawGame, 1000 / speed); //update screen 7 times a second
-  
-  clearScreen()
 }
 
 function clearScreen() {
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight); //color start from 0px left, right to canvas width and canvas height
+}
+
+function drawSnake() {
+  ctx.fillStyle = "orange";
+  ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
 
 drawGame();
